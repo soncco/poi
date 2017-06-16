c5622c42eeacafc8ef8b414c23cbabaeb5a107c4
brau <soncco@gmail.com>
2017-06-16 10:35:18 -0500

    Admin planes

--- plan/admin.py
+++ plan/admin.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-# Register your models here.
+from models import *
+
+
+class PlanAdmin(admin.ModelAdmin):
+  list_display = ('numero', 'unidad_organica', 'responsable', 'anio', 'aprobado')
+  search_fields = ['numero', 'responsable']
+  list_filter = ('anio', 'unidad_organica', 'periodo', 'aprobado',)
+
+admin.site.register(Plan, PlanAdmin)

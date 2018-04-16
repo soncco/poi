9a6c07b9555ef12e5c13bb1516200df6e063742b
Braulio Soncco <soncco@gmail.com>
2018-04-16 07:54:00 -0500

    m

--- poi/settings.py
+++ poi/settings.py
@@ -144,5 +144,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'no-reply@abastecimiento.pe'
-EMAIL_HOST_PASSWORD = 'valoisponten4'
 DEFAULT_FROM_EMAIL = 'no-reply@abastecimiento.pe'
